8ad7bf56bd364b9d23e781fa208fd1a96614b375
AutoAFK <ranhaim85@gmail.com>
2026-04-05 00:14:01 +0300

    better handler for each page

--- src/index.js
+++ src/index.js
@@ -4,4 +4,21 @@ import "./styles/main.css";
 
 const app = document.querySelector("#content");
 
+const navbar = document.querySelector("nav");
+
+const pages = {
+  "home" : MainPage,
+  // "menu" : MenuPage,
+  // "contact" : ContactPage,
+}
+
+navbar.addEventListener("click", (event) => {
+  const target = event.target;
+
+  if(target.tagName !== "BUTTON") return;
+  app.innerHTML = "";
+  // Call the page and pass it the app container;
+  pages[target.id](app);
+})
+
 MainPage(app);
