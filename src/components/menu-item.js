88fa8db94c2644afe62fea3edd566eeaa3dcd51f
AutoAFK <ranhaim85@gmail.com>
2026-04-04 21:49:34 +0300

    menu items creation

---
+++ src/components/menu-item.js
@@ -0,0 +1,36 @@
+import * as elements from "../elements/elements.js";
+
+export class MenuItem {
+  constructor(name, description, price) {
+    this.name = name;
+    this.description = description;
+    this.price = price;
+  }
+
+  render = () => {
+    const container = elements.createTag({
+      element: "div",
+      classList: ["menu-item"],
+    });
+
+    const header = elements.createTag({
+      element: "h2",
+      textContent: this.name,
+      classList: ["item-header"],
+    });
+
+    const body = elements.createTag({
+      element: "p",
+      textContent: this.description,
+      classList: ["item-body"],
+    });
+
+    const price = elements.createTag({
+      element: "p",
+      textContent: this.price,
+      classList: ["item-price"],
+    });
+
+    elements.appendChild(container, [header, body, price]);
+  };
+}
