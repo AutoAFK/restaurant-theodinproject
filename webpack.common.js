9402c4b368fe45510fda078647f41165638f887d
AutoAFK <ranhaim85@gmail.com>
2026-04-06 21:29:07 +0300

    Seperated the config files of webpack

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,6 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import path from "node:path";
 
 export default {
-  mode: "development",
   entry: "./src/index.js",
   experiments: {
     outputModule: true,
@@ -13,13 +12,11 @@ export default {
     clean: true,
     module: true,
   },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/templates/template.html"],
-  },
+  
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/templates/template.html",
+      scriptLoading: "module",
     }),
   ],
   module: {
